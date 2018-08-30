e296e298a243c9ca04360beecd0699556133e459
braj011 <bhagyaraj01@gmail.com>
2018-08-30 23:08:56 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
-  def question?
 
+  # it "returns true if the string that you are calling this method on ends in a period." do
+  #   expect("Hi, my name is Sophie.".sentence?).to eq(true)
+  # end
+  #
+  # it "returns false if the string that you are calling this method on does NOT end in a period." do
+  #   expect("Hi, my name is Sophie".sentence?).to eq(false)
+  # end
+
+  def question?
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
